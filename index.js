ceb5437d509cff573f09d5ba5eaf62e7e9bcb512
vipinvijay1986 <vipinpublicdomain@gmail.com>
2020-05-20 17:54:06 +0530

    redux-thunk,axios

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const redux =require('redux')
+const reduxLogger =require('redux-logger')
+
 const createStore = redux.createStore
 const combineReducers = redux.combineReducers
+const applyMiddleware = redux.applyMiddleware
+const logger =reduxLogger.createLogger()
 console.log("From index.js")
 const BUY_CAKE ='BUY_CAKE';
 const BUY_ICECREAM ='BUY_ICECREAM'
@@ -72,9 +76,10 @@ const rootReducer=combineReducers({
     cake : cakeReducer,
     iceCream : icecreamReducer
 })
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(logger));
 console.log('initial state ',store.getState())
-const unsubscribe = store.subscribe(()=> console.log('updated score ',store.getState()))
+//const unsubscribe = store.subscribe(()=> console.log('updated score ',store.getState()))
+const unsubscribe = store.subscribe(()=>{})
 store.dispatch(buyCake())
 store.dispatch(buyCake())
 store.dispatch(buyCake())
